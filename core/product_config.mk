f1fd1c5e664e903c7df76d24ca7225733530c462
Steve Kondik <shade@chemlab.org>
2012-11-18 18:43:17 -0800

    build: Fix fastpath code for CM_BUILD
     * We need to adjust to the new validations
    
    Change-Id: I9066d4f437beb9597027ee6bbb52504b5e7e84c5

--- core/product_config.mk
+++ core/product_config.mk
@@ -186,7 +186,7 @@ all_product_configs := $(call get-product-makefiles,\
     $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
 else
   ifneq ($(CM_BUILD),)
-    all_product_configs := device/*/$(CM_BUILD)/cm.mk
+    all_product_configs := $(shell ls device/*/$(CM_BUILD)/cm.mk)
   else
     # Read in all of the product definitions specified by the AndroidProducts.mk
     # files in the tree.
@@ -194,6 +194,7 @@ else
   endif # CM_BUILD
 endif
 
+ifeq ($(CM_BUILD),)
 # Find the product config makefile for the current product.
 # all_product_configs consists items like:
 # <product_name>:<path_to_the_product_makefile>
@@ -216,6 +217,10 @@ $(foreach f, $(all_product_configs),\
 _cpm_words :=
 _cpm_word1 :=
 _cpm_word2 :=
+else
+    current_product_makefile := $(strip $(all_product_configs))
+    all_product_makefiles := $(strip $(all_product_configs))
+endif
 current_product_makefile := $(strip $(current_product_makefile))
 all_product_makefiles := $(strip $(all_product_makefiles))
 
